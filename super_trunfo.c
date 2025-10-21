3de60a63f767e39f5587780b551f31d93b7ad3a4
Maikel <maikelfullstack@gmail.com>
2025-10-21 17:14:25 -0300

    Nivel aventureiro implementado.

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,24 +1,58 @@
 #include <stdio.h>
 
-// Criar um programa em C que permita ao usuário inserir os dados de duas cartas do Super Trunfo. Para cada carta, o usuário deverá fornecer as seguintes informações:
+ 
 
-// Estado: Uma letra de 'A' a 'H' (representando um dos oito estados). Tipo: char
-//  Código da Carta: A letra do estado seguida de um número de 01 a 04 (ex: A01, B03). Tipo: char[] (um array de caracteres, ou string)
- // Nome da Cidade: O nome da cidade. Tipo: char[] (string)
- // População: O número de habitantes da cidade. Tipo: int
- // Área (em km²): A área da cidade em quilômetros quadrados. Tipo: float
- // PIB: O Produto Interno Bruto da cidade. Tipo: float
- // Número de Pontos Turísticos: A quantidade de pontos turísticos na cidade. Tipo: int
- // Após o usuário inserir os dados de cada carta, seu programa deve exibir na tela as informações cadastradas, de forma organizada e legível. Para cada carta, imprima cada informação em uma linha separada, com uma descrição clara. Por exemplo:
+// Calcular a Densidade Populacional: Divida a população da cidade pela sua área. O resultado será a densidade populacional, que representa o número de habitantes por quilômetro quadrado. Armazene esse valor em uma variável do tipo float.
+ 
+// Calcular o PIB per Capita: Divida o PIB da cidade pela sua população. O resultado será o PIB per capita, que indica a riqueza média por pessoa na cidade. Armazene esse valor em uma variável do tipo float.
+ 
+// Exibir os Resultados: Além de exibir as informações lidas do usuário (como no nível básico), seu programa também deve exibir a densidade populacional e o PIB per capita calculados para cada cidade. Formate os valores de ponto flutuante com duas casas decimais.
+ 
+
+// Exemplo de Saída:
+
 
 // Carta 1:
+
 // Estado: A
+
 // Código: A01
+
 // Nome da Cidade: São Paulo
+
 // População: 12325000
+
 // Área: 1521.11 km²
+
 // PIB: 699.28 bilhões de reais
+
 // Número de Pontos Turísticos: 50
+
+// Densidade Populacional: 8102.47 hab/km²
+
+// PIB per Capita: 56724.32 reais
+
+ 
+
+// Carta 2:
+
+// Estado: B
+
+// Código: B02
+
+// Nome da Cidade: Rio de Janeiro
+
+// População: 6748000
+
+// Área: 1200.25 km²
+
+// PIB: 300.50 bilhões de reais
+
+// Número de Pontos Turísticos: 30
+
+// Densidade Populacional: 5622.24 hab/km²
+
+// PIB per Capita: 44532.91 reais
 
  
 int main() {
@@ -27,13 +61,24 @@ int main() {
     int carta1_populacao, carta1_pontos_turisticos;
     float carta1_area;
     double carta1_pib;
+  
+ 
 
     //carta2
     char carta2_estado, carta2_codigo[4], carta2_cidade[50];
     int carta2_populacao, carta2_pontos_turisticos;
-    float carta2_area;
+    float carta2_area,
     double carta2_pib;
 
+  // operacoes densidade
+ float densidade_carta1 = carta1_populacao / carta1_area;
+ float densidade_carta2= carta2_populacao / carta2_area;
+
+  //Calcular o PIB per Capita
+ float PPC1 = carta1_pib / carta1_populacao;
+ float ppc2 = carta2_pib / carta2_populacao;
+ 
+  
     printf("Hello, Super Trunfo!\n");
     printf("Insira os dados da Carta 1:\n");
     printf("Estado (A-H): ");
@@ -77,6 +122,8 @@ int main() {
     printf("Área: %.2f km²",carta1_area);
     printf("PIB: %.2f bilhões de reais",carta1_pib);
     printf("Número de Pontos Turísticos: %d",carta1_pontos_turisticos);
+    printf("Densidade populaciona : %.2f", densidade_carta1);
+    printf("PIB per capta :  %.2f", PPC1);
 
     printf("\nCarta 2:\n");
     printf("Estado: %c",carta2_estado);
@@ -86,5 +133,11 @@ int main() {
     printf("Área: %.2f km²",carta2_area);
     printf("PIB: %.2f bilhões de reais",carta2_pib);
     printf("Número de Pontos Turísticos: %d",carta2_pontos_turisticos);
+    printf("Densidade populaciona : %.2f", densidade_carta2);
+    printf("PIB per capta :  %.2f", PPC2);
+
+ 
+   
+
     return 0;
 }
